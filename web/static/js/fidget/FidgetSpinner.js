dd1a4cb72f998d92a0262692896f2ef68bbe64c7
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-07-20 14:15:58 +0200

    Fidget is draggable

--- web/static/js/fidget/FidgetSpinner.js
+++ web/static/js/fidget/FidgetSpinner.js
@@ -1,4 +1,5 @@
 import { TimelineMax, TweenLite, Power0 } from 'gsap'
+import Draggable from 'gsap/Draggable'
 
 export default class {
   constructor(channel) {
@@ -6,15 +7,27 @@ export default class {
     this.fidget = document.querySelector('.fidget-spinner')
     this.channel = channel
     this.speed = 0
-    this.position = 0
+    this.x = 0
+    this.y = 0
   }
 
   initialize() {
+    Draggable.create(this.fidget, { 
+      type: 'x,y',
+      edgeResistance: 0.65,
+      bounds: '.container',
+      throwProps: true,
+      onPress: this.socketSpin.bind(this),
+      onDrag: this.socketMove.bind(this)
+    })
     this.fidget.addEventListener('mousedown', this.socketSpin.bind(this))
     this.timeline.to(this.fidget, 2, { rotation: 360, ease: Power0.easeNone })
   }
 
-  socketSpin(event) { this.channel.push('spin', { body: 'spinner' }) }
+
+  socketMove({ layerX: x, layerY: y }) { this.channel.push('move', { x, y }) }
+
+  socketSpin() { this.channel.push('spin') }
 
   flash() {
     TweenLite.to(this.fidget, 0.250, { scale: 0.1, filter: 'brightness(2)' })

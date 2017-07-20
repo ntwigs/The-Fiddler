75721ca6b5d940f6411ec7e5e1723cb8b75d5d47
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-07-20 09:56:14 +0200

    Tweenflash fidget

--- web/static/js/fidget/FidgetSpinner.js
+++ web/static/js/fidget/FidgetSpinner.js
@@ -1,7 +1,7 @@
 import { TimelineMax, TweenLite, Power0 } from 'gsap'
 
 export default class {
-  constructor (channel) {
+  constructor(channel) {
     this.timeline = new TimelineMax({ repeat: -1, repeatDelay: 0 })
     this.fidget = document.querySelector('.fidget-spinner')
     this.channel = channel
@@ -9,14 +9,19 @@ export default class {
     this.position = 0
   }
 
-  initialize () {
+  initialize() {
     this.fidget.addEventListener('mousedown', this.socketSpin.bind(this))
     this.timeline.to(this.fidget, 2, { rotation: 360, ease: Power0.easeNone })
   }
 
-  socketSpin (event) { this.channel.push('spin', { body: 'spinner' }) }
+  socketSpin(event) { this.channel.push('spin', { body: 'spinner' }) }
 
-  increaseSpeed () {
+  flash() {
+    TweenLite.to(this.fidget, 0.250, { scale: 0.1, filter: 'brightness(2)' })
+    setTimeout(() => TweenLite.to(this.fidget, 0.250, { scale: 1, filter: 'brightness(1)' }), 100)
+  }
+
+  increaseSpeed() {
     TweenLite.to(this.timeline, 0, {
       timeScale: this.speed
     })

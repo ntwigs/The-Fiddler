ba3abc5131cc764e934d3f68ba6839240bda6bd9
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-07-20 09:55:45 +0200

    Play animation on click

--- web/static/js/fidget/fidgetController.js
+++ web/static/js/fidget/fidgetController.js
@@ -4,12 +4,13 @@ export default channel => {
   const fidgetSpinner = new FidgetSpinner(channel)
   fidgetSpinner.initialize()
 
-  channel.on('initialize', setSpeed.bind(this, fidgetSpinner))
-  channel.on('spin', setSpeed.bind(this, fidgetSpinner))
-  channel.on('update', setSpeed.bind(this, fidgetSpinner))
+  channel.on('initialize', setSpeed.bind(this, fidgetSpinner, false))
+  channel.on('spin', setSpeed.bind(this, fidgetSpinner, true))
+  channel.on('update', setSpeed.bind(this, fidgetSpinner, false))
 }
 
-const setSpeed = (fidgetSpinner, { body: speed }) => {
+const setSpeed = (fidgetSpinner, shouldFlash, { body: speed }) => {
   fidgetSpinner.speed = speed
   fidgetSpinner.increaseSpeed()
+  if (shouldFlash) fidgetSpinner.flash()
 }

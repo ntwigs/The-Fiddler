e09b3c0fe1de0fd549fd0de87f9afca0848bc546
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-07-20 14:14:54 +0200

    Sets position in state

--- lib/take_it_for_a_spin/state.ex
+++ lib/take_it_for_a_spin/state.ex
@@ -2,8 +2,11 @@ defmodule TakeItForASpin.State do
   @speed 0.2
 
   def start_link do
-    Agent.start_link(fn -> %{ speed: 0 } end, name: :state)
+    Agent.start_link(fn -> %{ speed: 0, x: 0, y: 0 } end, name: :state)
   end
+
+
+  # Speed State
 
   def increase_speed do
     if (get_speed < 10) do
@@ -21,17 +24,36 @@ defmodule TakeItForASpin.State do
     end
   end
 
-  defp calculate_speed_increase(body) do
-    speed = body.speed + @speed
-    { speed, %{ speed: speed } }
+  defp calculate_speed_increase map do
+    speed = map.speed + @speed
+    { speed, %{ map | speed: speed } }
   end
 
-  defp calculate_speed_decrease(body) do
-    speed = body.speed - 0.1
-    { speed, %{ speed: speed } }
+  defp calculate_speed_decrease map do
+    speed = map.speed - 0.1
+    { speed, %{ map | speed: speed } }
   end
 
   def get_speed do
     Agent.get(:state, &(&1.speed))
   end
+
+
+  # Position State
+
+  def set_position position do
+    x = position["x"]
+    y = position["y"]
+    Agent.get_and_update(:state, &calculate_position(&1, x, y))
+  end
+
+  def calculate_position(previous_state, x, y) do
+    new_value = %{ previous_state | x: x, y: y }
+    { new_value, new_value }
+  end
+
+  def get_position do
+    Agent.get(:state, &(&1))
+  end
+
 end

806e0426113a10f5b34145431d7818514eff3eb1
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-07-20 16:45:40 +0200

    Added setters to FidgetController

--- web/static/js/fidget/fidgetController.js
+++ web/static/js/fidget/fidgetController.js
@@ -1,10 +1,21 @@
 import FidgetSpinner from './FidgetSpinner'
 
-export default channel => {
+const setSpeed = (fidgetSpinner, shouldFlash, { speed }) => {
+  fidgetSpinner.setSpeed(speed)
+  fidgetSpinner.increaseSpeed()
+  if (shouldFlash) fidgetSpinner.flash()
+}
+
+const setPosition = (fidgetSpinner, { position }) => {
+  fidgetSpinner.setPosition(position)
+  fidgetSpinner.move()
+}
+
+export default (channel) => {
   const fidgetSpinner = new FidgetSpinner(channel)
   fidgetSpinner.initialize()
 
-  channel.on('initialize', intialValues => {
+  channel.on('initialize', (intialValues) => {
     setSpeed(fidgetSpinner, false, intialValues)
     setPosition(fidgetSpinner, intialValues)
   })
@@ -12,17 +23,4 @@ export default channel => {
   channel.on('spin', setSpeed.bind(this, fidgetSpinner, true))
   channel.on('update', setSpeed.bind(this, fidgetSpinner, false))
   channel.on('move', setPosition.bind(this, fidgetSpinner))
-}
-
-const setSpeed = (fidgetSpinner, shouldFlash, { speed }) => {
-  fidgetSpinner.speed = speed
-  fidgetSpinner.increaseSpeed()
-  if (shouldFlash) fidgetSpinner.flash()
-}
-
-const setPosition = (fidgetSpinner, { position }) => {
-  const { x, y } = position
-  fidgetSpinner.x = x 
-  fidgetSpinner.y = y 
-  fidgetSpinner.move()
 }

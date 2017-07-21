e47714750cef91c0c5620fe1742802d2ee206b8d
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-07-22 00:14:51 +0200

    Removed text code from fidget controller

--- web/static/js/fidget/fidgetController.js
+++ web/static/js/fidget/fidgetController.js
@@ -1,4 +1,3 @@
-import { Presence } from 'phoenix'
 import FidgetSpinner from './FidgetSpinner'
 
 const setSpeed = (fidgetSpinner, shouldFlash, { speed }) => {
@@ -15,26 +14,6 @@ const setPosition = (fidgetSpinner, { position }) => {
 export default (channel) => {
   const fidgetSpinner = new FidgetSpinner(channel)
   fidgetSpinner.initialize()
-
-  // Move section to new FIle
-  let presences = {}
-
-  const setUsers = ({ user }) => {
-    const amount = user.metas.length
-    console.log(amount)
-  }
-
-  channel.on('presence_state', (state) => {
-    presences = Presence.syncState(presences, state)
-    setUsers(presences)
-  })
-  channel.on('presence_diff', (state) => {
-    presences = Presence.syncDiff(presences, state)
-    setUsers(presences)
-  })
-
-
-  // Hit
 
   channel.on('initialize', (intialValues) => {
     setSpeed(fidgetSpinner, false, intialValues)

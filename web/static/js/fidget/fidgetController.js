161d8898b220313436b6044e46646befa28ea83d
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-07-21 18:06:19 +0200

    Added methods for presence

--- web/static/js/fidget/fidgetController.js
+++ web/static/js/fidget/fidgetController.js
@@ -1,3 +1,4 @@
+import { Presence } from 'phoenix'
 import FidgetSpinner from './FidgetSpinner'
 
 const setSpeed = (fidgetSpinner, shouldFlash, { speed }) => {
@@ -14,6 +15,26 @@ const setPosition = (fidgetSpinner, { position }) => {
 export default (channel) => {
   const fidgetSpinner = new FidgetSpinner(channel)
   fidgetSpinner.initialize()
+
+  // Move section to new FIle
+  let presences = {}
+
+  const setUsers = ({ user }) => {
+    const amount = user.metas.length
+    console.log(amount)
+  }
+
+  channel.on('presence_state', (state) => {
+    presences = Presence.syncState(presences, state)
+    setUsers(presences)
+  })
+  channel.on('presence_diff', (state) => {
+    presences = Presence.syncDiff(presences, state)
+    setUsers(presences)
+  })
+
+
+  // Hit
 
   channel.on('initialize', (intialValues) => {
     setSpeed(fidgetSpinner, false, intialValues)
